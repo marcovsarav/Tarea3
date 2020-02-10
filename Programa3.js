6ae65f33bc0ad699f3f9610bfe1ba8a0083f0203
marcovsarav <maco.top83@gmail.com>
2020-02-09 21:45:09 -0600

    Agregar archivo de prueba

---
+++ Programa3.js
@@ -0,0 +1,16 @@
+var mes = 10;
+if (mes >=1 && mes <=2){
+console.log('Es Invierno');
+}else if(mes >=3 && mes <=5){
+console.log('Es primavera');
+}else if(mes >=6 && mes <=8){
+console.log('Es Verano');
+}else if(mes >=9 && mes <=11){
+console.log('Es otoño');
+}else if(mes ==12){
+console.log('Es Invierno');
+}else {
+console.log(' ¡Adevertencia!  El número no representa ningún mes del año ' );
+}
+    console.log(carnet);
+    console.log(nombre)
